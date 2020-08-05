0c31702fc21cba5dd880d1acec84f7255b70df2e
wjh818 <wjh818@naver.com>
2020-08-05 11:46:31 +0900

    상수 연습

--- src/com/javaex/basic/TypeEx.java
+++ src/com/javaex/basic/TypeEx.java
@@ -7,7 +7,23 @@ public class TypeEx {
 //		intLongTest();
 //		floatDoubleTest();
 //		booleanEx();
-		charEx();
+//		charEx();
+		constantEx();
+	}
+	
+	//상수 연습(constant)
+	public static void constantEx() {
+		//변하지 않는 데이터
+		//final을 선언부 앞에 붙이면 된다
+		//상수 식별자는 모두 대문자, 여러 단어 결합시 단어 사이에 _
+		final double PI = 3.14159; 
+		final int SPEED_LIMIT = 110;
+		
+		System.out.println("PI = " + PI);
+		System.out.println("제한속도 = " + SPEED_LIMIT);
+		
+//		Speed_LIMIT = 80; //상수이므로 변경 불가하게 된다
+		System.out.println("제한속도 = " + SPEED_LIMIT);
 	}
 
 	//char형 연습
