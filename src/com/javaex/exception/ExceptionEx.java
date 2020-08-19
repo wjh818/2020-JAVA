4d89e2310494509d5675580bb5efd82ec5c5da2d
wjh818 <wjh818@naver.com>
2020-08-19 11:37:20 +0900

    사용자 정의 예외의 처리

--- src/com/javaex/exception/ExceptionEx.java
+++ src/com/javaex/exception/ExceptionEx.java
@@ -1,5 +1,6 @@
 package com.javaex.exception;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,9 +9,30 @@ public class ExceptionEx {
 	public static void main(String[] args) {
 //		arithmeticExceptionEx();
 //		indexOutofBoundEx();
-		nullPointerException();
+//		nullPointerException();
+		customExceptionEx();
 	}
 
+	private static void customExceptionEx() {
+		// 사용자 정의 예외의 활용
+		ThrowExcept ex = new ThrowExcept();
+		try {
+//			ex.executeCheckedException(); // 체크드 익셉션 발생
+			ex.executeUnchedckedException(); //언체크드 익셉션 발생
+			System.out.println("10/0 = " + ex.divide(10,0));
+//		} catch (IOException e) {
+//			System.err.println("예외 메시지: " + e.getMessage());
+		}catch (CustomArithmeticException e) {
+			// 본 예외는 RuntimeException의 자손이므로
+			// RuntumeException 보다 위에서 catch 해 줘야 함
+			System.err.println("사용자 정의 예외 메시지: "+ e.getMessage());
+			System.err.printf("예외상황 발생시 num1 = %d, num2 = %d%n", e.getNum1(), e.getNum2());
+		} catch (RuntimeException e) {
+			System.err.println("런타임 예외 메시지: " + e.getMessage());
+		} 
+	}
+	
+	
 	// 가장 흔히 발생하는 예외
 	private static void nullPointerException() {
 		String s = "Hello"; // 참조 객체
---
+++ src/com/javaex/exception/ThrowExcept.java
@@ -0,0 +1,33 @@
+package com.javaex.exception;
+
+import java.io.IOException;
+
+// 메서드에서 강제 예외 발생
+public class ThrowExcept {
+	
+	public void executeCheckedException() throws IOException {
+		System.out.println("체크드 예외 발생!");
+		// 강제 예외 발생
+		throw new IOException("강제 IO 예외");
+	}
+	
+	public void executeUnchedckedException() throws RuntimeException {
+		System.out.println("언체크드 예외 발생!");
+		// 언테크드 예외는 대부분 RuntumeException의 자식 클래스
+		throw new RuntimeException("강제 RuntimeException");
+	}
+	
+	public double divide(int num1, int num2) throws CustomArithmeticException {
+		double result = 0;
+		
+		try {
+			result = num1 / num2;
+		} catch (ArithmeticException e) {
+			// 일반적 예외 발생시 보다 구체적인 사용자 정의 예외로 전환하여 throw 하는것이
+			// 가독성, 코드 디버그 작업에서 유리해짐
+			throw new CustomArithmeticException("사용자 정의 예외", num1, num2);
+		}
+		
+		return result;
+	}
+}
