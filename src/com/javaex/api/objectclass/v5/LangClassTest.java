ec6bcd932b45c9270e1e692815de8522c62d0e37
wjh818 <wjh818@naver.com>
2020-08-20 11:42:24 +0900

    Language API

---
+++ src/com/javaex/api/objectclass/v5/LangClassTest.java
@@ -0,0 +1,23 @@
+package com.javaex.api.objectclass.v5;
+
+// v5. 참조 타입 필드를 가진 클래스의 깊은 복제 테스트 (코드 수정 없음)
+public class LangClassTest {
+	
+	public static void main(String[] args) {
+		Scoreboard s1 = new Scoreboard(new int[] {
+			10, 20, 30, 40
+		});
+		System.out.println("s1 = " + s1);
+		
+		// 복제
+		Scoreboard s2 = s1.getClone();
+		System.out.println("s2 = " + s2);
+		
+		// 두 객체는 같은 객체인가?
+		System.out.println("s1 == s2 ? " + (s1 == s2));
+		// 내부 참조 타입은 같은 객체인가?
+		System.out.println("s1.scores == s2.scores ? " + (s1.getScores() == s2.getScores()));
+		// 객체는 복제되었으나 내부 참조 타입 객체는 동일하다 (얕은 복제의 특징)
+		// 객체 복제되고, 내부 참조 필드도 복제되어 분리되었다 (깊은 복제)  ***중요***
+	}
+}
