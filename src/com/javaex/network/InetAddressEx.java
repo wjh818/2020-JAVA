2611554ceefe5478ebcef11efb4b65418cb216bb
wjh818 <wjh818@naver.com>
2020-08-27 10:58:23 +0900

    Thread, InetAddress

--- src/com/javaex/network/InetAddressEx.java
+++ src/com/javaex/network/InetAddressEx.java
@@ -1,7 +1,7 @@
 package com.javaex.network;
 
 import java.net.InetAddress;
-import java.rmi.UnknownHostException;
+import java.net.UnknownHostException;
 
 // 인터넷 주소에 관련된 InetAddress 클래스 이해
 public class InetAddressEx {
@@ -9,6 +9,7 @@ public class InetAddressEx {
 	public static void main(String[] args) {
 		// 원격 서버의 IP 주소 확인
 		printServerIp("www.google.com");
+		printServerIp("www.naver.com");
 	}
 	
 	 private static void printServerIp(String hostname) {
@@ -16,6 +17,12 @@ public class InetAddressEx {
 			 // Local IP 확인
 			 InetAddress local = InetAddress.getLocalHost();
 			 System.out.println("Local IP: " + local.getHostAddress());
+			 
+			 // hostname 주소의 실제 IP를 확인
+			 InetAddress[] addrs = InetAddress.getAllByName(hostname);
+			 for (InetAddress remote: addrs) {
+				 System.out.println(hostname + ": " + remote.getHostAddress());
+			 }
 		 } catch (UnknownHostException e) {
 			 e.printStackTrace();
 		 }
